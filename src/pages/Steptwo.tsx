caa3a0896fb7765fb65eb5eabb80c94d3e84a5c1
Pi4a <piralith@outlook.fr>
2024-01-28 18:35:36 +0100

    adding code

--- src/pages/Steptwo.tsx
+++ src/pages/Steptwo.tsx
@@ -7,18 +7,22 @@ import Scissors from '../components/Scissors'
 import Rock from '../components/Rock'
 import './StepTwo.css'
 import Empty from '../components/Empty'
+import { Link } from 'react-router-dom'
 
 function Steptwo(props: any) {
     
   const choice  = useChoice()
   const updateChoice = useChoiceUpdate()
 
-function returnchoice(){
+  
   let chosen = choice?.choice
+
+function returnchoice(){
+
   if (chosen== "Scissors"){
     return <Scissors/>
   }
-  else if (chosen == "Rock"){
+  if (chosen == "Rock"){
     return <Rock/>
   }
   else if (chosen == "Paper"){
@@ -26,8 +30,10 @@ function returnchoice(){
   }
 }
 
+let house = Math.floor(Math.random()* 3) +1
+
 function houseChoice(){
-  let house = Math.floor(Math.random()* 3) +1
+ 
   if(house==1){
     return <Scissors/>
   }
@@ -39,19 +45,66 @@ function houseChoice(){
   }
 }
 
+function winOrLose(){
+  let victory = ''
+  if(chosen=='Scissors'){
+    if(house==1){
+      return victory = "IT'S A TIE!"
+    }
+    if(house==2){
+      return victory="YOU LOSE"
+    }
+    if(house==3){
+      return victory= "YOU WIN"
+    }}
+    else if(chosen=='Rock'){
+    if(house==1){
+      return victory = "YOU LOSE"
+    }
+    if(house==2){
+      return victory="IT'S A TIE!"
+    }
+    if(house==3){
+      return victory= "YOU LOSE"
+    }
+  }else if(chosen=='Paper'){
+    if(house==1){
+      return victory = "YOU LOSE"
+    }
+    if(house==2){
+      return victory="YOU WIN"
+    }
+    if(house==3){
+      return victory= "IT'S A TIE!"
+    }
+  }
+  
+}
 
-  return  (
+
+
+  return  (<>
     <div className='choicecontainer'> 
         <div className='twofirsts'>
-         <div className='pick'> {returnchoice()}
+         <div className='pick'> <div className='glow'></div>{returnchoice()}
           <p>YOU PICKED</p>
           </div>
           <div className='pick'>
-              {houseChoice()}<p>THE HOUSE PICKED</p>
+            
+              
+              
+              <div className="absolute"><Empty/></div>
+              <div className='house'>{houseChoice()}</div>
+              <p>THE HOUSE PICKED</p>
                   </div>
                   </div>
-        </div>    
-    
+                
+        </div>  
+        <div className='winorloss'>
+        <h1 className='delay'>{winOrLose()}</h1>
+        <Link to="/"><button className='playagain'><p>PLAY AGAIN</p></button></Link>
+      </div>  
+    </>
   )
 }
 
